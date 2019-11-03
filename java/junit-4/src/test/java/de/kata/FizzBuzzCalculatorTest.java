7d4fcf0accb4f6e904053befe8e1166ed19c4bc8
Felix <felix.boeer@mailbox.org>
2019-11-03 19:07:12 +0100

    Add template classes

---
+++ java/junit-4/src/test/java/de/kata/FizzBuzzCalculatorTest.java
@@ -0,0 +1,12 @@
+package de.kata;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class FizzBuzzCalculatorTest {
+    @Test
+    public void testAssertJIsAlwaysBetter() {
+        assertThat(true).isTrue();
+    }
+}
