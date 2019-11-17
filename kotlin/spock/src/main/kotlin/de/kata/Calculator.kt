b5f717250e670f7786845ec99e0329e3028bfb7f
Felix <felix.boeer@mailbox.org>
2019-11-17 12:20:41 +0100

    added

---
+++ kotlin/spock/src/main/kotlin/de/kata/Calculator.kt
@@ -0,0 +1,3 @@
+package de.kata
+
+val helloWorld = "This is Kotlin Hello World"
---
+++ kotlin/spock/src/test/groovy/de/kata/CalculatorTest.groovy
@@ -0,0 +1,11 @@
+package de.kata
+
+import spock.lang.Specification
+
+class CalculatorTest extends Specification {
+
+    def "some Test"() {
+        expect:
+        true == true
+    }
+}
